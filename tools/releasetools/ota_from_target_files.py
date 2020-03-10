fdfda928009e51376cef0e309ca155f4e21e7547
ArtOS-Dev <ArtStarOS.Project@gmail.com>
2020-03-10 08:38:43 +0000

    ArtStarOS - ASOP

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -1285,14 +1285,22 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   android_version = target_info.GetBuildProp("ro.build.version.release")
   build_id = target_info.GetBuildProp("ro.build.id")
-  build_date = target_info.GetBuildProp("org.pixelexperience.build_date")
+  build_date = target_info.GetBuildProp("ro.artstar.build_date")
   security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
-  device = target_info.GetBuildProp("org.pixelexperience.device")
+  device = target_info.GetBuildProp("ro.artstar.device")
 
-  script.Print("----------------------------------------------");
-  script.Print("              Pixel Experience");
-  script.Print("               by jhenrique09");
-  script.Print("----------------------------------------------");
+  script.Print("-------------------------------------------------------");
+  script.Print("                _    _____ _              ____   _____ ");
+  script.Print("     /\        | |  / ____| |            / __ \ / ____|");
+  script.Print("    /  \   _ __| |_| (___ | |_ __ _ _ __| |  | | (___  ");
+  script.Print("   / /\ \ | '__| __|\___ \| __/ _` | '__| |  | |\___ \ ");
+  script.Print("  / ____ \| |  | |_ ____) | || (_| | |  | |__| |____) |");
+  script.Print(" /_/    \_\_|   \__|_____/ \__\__,_|_|   \____/|_____/ ");
+  script.Print("						       ");
+  script.Print("-------------------------------------------------------");
+  script.Print("                  	ArtStarOS                      ");
+  script.Print("                     by ArtStar Team                   ");
+  script.Print("-------------------------------------------------------");
   script.Print(" Android version: %s"%(android_version));
   script.Print(" Build id: %s"%(build_id));
   script.Print(" Build date: %s"%(build_date));
@@ -2061,21 +2069,29 @@ else if get_stage("%(bcb_dev)s") != "3/3" then
   script.Print("Target: {}".format(target_info.fingerprint))
 
   android_version = target_info.GetBuildProp("ro.build.version.release")
-  device = target_info.GetBuildProp("org.pixelexperience.device")
+  device = target_info.GetBuildProp("ro.artstar.device")
 
   prev_build_id = source_info.GetBuildProp("ro.build.id")
   build_id = target_info.GetBuildProp("ro.build.id")
 
-  prev_build_date = source_info.GetBuildProp("org.pixelexperience.build_date")
-  build_date = target_info.GetBuildProp("org.pixelexperience.build_date")
+  prev_build_date = source_info.GetBuildProp("ro.artstar.build_date")
+  build_date = target_info.GetBuildProp("ro.artstar.build_date")
 
   prev_security_patch = source_info.GetBuildProp("ro.build.version.security_patch")
   security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
 
-  script.Print("----------------------------------------------");
-  script.Print("              Pixel Experience");
-  script.Print("               by jhenrique09");
-  script.Print("----------------------------------------------");
+  script.Print("-------------------------------------------------------");
+  script.Print("                _    _____ _              ____   _____ ");
+  script.Print("     /\        | |  / ____| |            / __ \ / ____|");
+  script.Print("    /  \   _ __| |_| (___ | |_ __ _ _ __| |  | | (___  ");
+  script.Print("   / /\ \ | '__| __|\___ \| __/ _` | '__| |  | |\___ \ ");
+  script.Print("  / ____ \| |  | |_ ____) | || (_| | |  | |__| |____) |");
+  script.Print(" /_/    \_\_|   \__|_____/ \__\__,_|_|   \____/|_____/ ");
+  script.Print("						       ");
+  script.Print("-------------------------------------------------------");
+  script.Print("                  	ArtStarOS                      ");
+  script.Print("                     by ArtStar Team                   ");
+  script.Print("-------------------------------------------------------");
   script.Print(" Android version: %s"%(android_version));
   if prev_build_id != build_id:
     script.Print(" Build id: %s -> %s"%(prev_build_id, build_id));
@@ -2599,21 +2615,29 @@ def WriteIncrementalOTAPackage(target_zip, source_zip, output_file):
   device_specific.IncrementalOTA_Assertions()
 
   android_version = target_info.GetBuildProp("ro.build.version.release")
-  device = target_info.GetBuildProp("org.pixelexperience.device")
+  device = target_info.GetBuildProp("ro.artstar.device")
 
   prev_build_id = source_info.GetBuildProp("ro.build.id")
   build_id = target_info.GetBuildProp("ro.build.id")
 
-  prev_build_date = source_info.GetBuildProp("org.pixelexperience.build_date")
-  build_date = target_info.GetBuildProp("org.pixelexperience.build_date")
+  prev_build_date = source_info.GetBuildProp("ro.artstar.build_date")
+  build_date = target_info.GetBuildProp("ro,artstar.build_date")
 
   prev_security_patch = source_info.GetBuildProp("ro.build.version.security_patch")
   security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
 
-  script.Print("----------------------------------------------");
-  script.Print("              Pixel Experience");
-  script.Print("               by jhenrique09");
-  script.Print("----------------------------------------------");
+  script.Print("-------------------------------------------------------");
+  script.Print("                _    _____ _              ____   _____ ");
+  script.Print("     /\        | |  / ____| |            / __ \ / ____|");
+  script.Print("    /  \   _ __| |_| (___ | |_ __ _ _ __| |  | | (___  ");
+  script.Print("   / /\ \ | '__| __|\___ \| __/ _` | '__| |  | |\___ \ ");
+  script.Print("  / ____ \| |  | |_ ____) | || (_| | |  | |__| |____) |");
+  script.Print(" /_/    \_\_|   \__|_____/ \__\__,_|_|   \____/|_____/ ");
+  script.Print("						       ");
+  script.Print("-------------------------------------------------------");
+  script.Print("                  	ArtStarOS                      ");
+  script.Print("                     by ArtStar Team                   ");
+  script.Print("-------------------------------------------------------");
   script.Print(" Android version: %s"%(android_version));
   if prev_build_id != build_id:
     script.Print(" Build id: %s -> %s"%(prev_build_id, build_id));
@@ -2653,11 +2677,11 @@ def WriteIncrementalOTAPackage(target_zip, source_zip, output_file):
     script.CheckAndUnmount("/product")
     script.Mount("/product")
 
-  error_msg = "Failed to apply update, please download full package at https://download.pixelexperience.org/" + device
+  error_msg = "Failed to apply update, please download full package at ArtStarOS" + device
 
   prop_path = "/" + GetSystemBasePath() + "/system/build.prop"
 
-  source_version_prop = "org.pixelexperience.version.display"
+  source_version_prop = "ro.artstar.version.display"
 
   source_version = os.path.splitext(os.path.basename(OPTIONS.incremental_source))[0]
 
